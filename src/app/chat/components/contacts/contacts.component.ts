6019095935f1aafdabec12c529c1d332e13ddbac
franklin barco <franklinbarco98@gmail.com>
2020-05-16 16:22:36 -0400

    adding visual components to the chat view

--- src/app/chat/components/contacts/contacts.component.ts
+++ src/app/chat/components/contacts/contacts.component.ts
@@ -7,9 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactsComponent implements OnInit {
 
+  listaDisponibles = false;
+  listaDesconectados = false;
+
+  usuarios = [
+    {
+      id: 1,
+      nombre: 'franklin',
+      estado: 'disponible'
+    }, {
+      id: 2,
+      nombre: 'miguel',
+      estado: 'disponible'
+    }, {
+      id: 3,
+      nombre: 'alondra',
+      estado: 'desconectado'
+    }, {
+      id: 4,
+      nombre: 'patricia',
+      estado: 'disponible'
+    }, {
+      id: 5,
+      nombre: 'maria',
+      estado: 'disponible'
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  dezplegarLista(estado) {
+    // console.log(estado)
+    if (estado == 'disponibles') {
+      this.listaDesconectados = false;
+      this.listaDisponibles = true;
+    } else {
+      if (estado == 'desconectados') {
+        this.listaDisponibles = false;
+        this.listaDesconectados = true;
+      }
+    }
   }
 
 }
